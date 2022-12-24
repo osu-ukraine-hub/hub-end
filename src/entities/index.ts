d0bab0a2a80a62088b211438807921bc2ac28341
alowave223 <belofost22@gmail.com>
2022-12-24 02:25:04 +0200

    Saving all votes with relations

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -2,8 +2,9 @@ import { MappoolMapEntity } from './mappoolMap.entity';
 import { PollEntity } from './poll.entity';
 import { TournamentEntity } from './tournament.entity';
 import { UserEntity } from './user.entity';
+import { VotesEntity } from './vote.entity';
 
-const entities = [UserEntity, TournamentEntity, MappoolMapEntity, PollEntity];
+const entities = [UserEntity, TournamentEntity, MappoolMapEntity, PollEntity, VotesEntity];
 
-export { UserEntity, TournamentEntity as Tournament, MappoolMapEntity as Mappool, PollEntity as Poll };
+export { UserEntity, TournamentEntity as Tournament, MappoolMapEntity as Mappool, PollEntity as Poll, VotesEntity as Vote};
 export default entities;
